b04fdf2c6d066025ace2e24e09774e24c8b9c1fb
дали <kodzoeva-radota@yandex.ru>
2023-02-07 20:34:28 +0300

    hw6

---
+++ homework6/task1/Program.cs
@@ -0,0 +1,50 @@
+﻿//  Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл 
+// пользователь.
+
+int [] CreatArray(string message)
+{
+    System.Console.WriteLine(message + ">");
+    int sizeMassiv = Convert.ToInt32(Console.ReadLine());
+    int[] massiv = new int[sizeMassiv];
+    return massiv;
+}
+
+int[] FillingArray (int[] baseArray, string message)
+{
+    for (int i = 0; i < baseArray.Length; i++)
+    {
+        System.Console.WriteLine(message + $">");
+        baseArray[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return  baseArray;
+}
+
+void DisplayElements(int[] baseArray)
+{
+    System.Console.WriteLine("Введенные числа");
+    for (int i = 0; i < baseArray.Length; i++)
+    {
+        System.Console.WriteLine($"{baseArray [i]}");
+    }
+}
+
+void FindNum(int[] baseArray)
+{
+    int count = 0;
+    for (int i = 0; i < baseArray.Length; i++)
+    {
+       if( baseArray[i] > 0) 
+       {
+        count ++;
+       }
+    }
+    Console.ForegroundColor = ConsoleColor.DarkGreen;
+    System.Console.WriteLine($"Количество значений больше нуля составляет {count}");
+}
+
+int[] baseArray = CreatArray("Введите количество чисел для ввода");
+string message = $"Введите число";
+FillingArray(baseArray,message);
+DisplayElements(baseArray);
+FindNum(baseArray);
